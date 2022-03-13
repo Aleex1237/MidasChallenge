31719320b92a629dd5a8d701264b8119788c3849
Aleex1237 <alexis10893@outlook.com>
2022-03-13 20:42:46 -0300

    added endpoints for register and login a user

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const usersController = require('../controllers/users');
+
 /* GET users listing. */
-router.get('/', () => console.log('hola'));
+router.post('/', usersController.register);
+
+router.post('/login', usersController.login);
 
 module.exports = router;
