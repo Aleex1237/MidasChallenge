68b24614be64618f62a107deb52753f273b47b12
Aleex1237 <alexis10893@outlook.com>
2022-03-13 20:43:29 -0300

    service for create and login user

---
+++ services/auth.js
@@ -0,0 +1,48 @@
+const usersRepository = require('../repositories/users');
+const bcryptService = require('./bcrypt');
+const jwtService = require('./jwt');
+const status = require('../constants/statusCodes');
+const messages = require('../constants/messages');
+
+const create = async (data) => {
+  const encryptedPassword = await bcryptService.encryptPassword(data.password);
+
+  const userBody = {
+    email: data.email,
+    password: encryptedPassword,
+  };
+
+  const user = await usersRepository.create(userBody);
+
+  if (!user) {
+    const error = new Error(messages.INTERNAL_ERROR);
+    error.status = status.INTERNAL_ERROR;
+    throw error;
+  }
+
+  return user;
+};
+
+const login = async (data) => {
+  const user = await usersRepository.getByEmail(data.email);
+
+  if (!user) {
+    const error = new Error(messages.EMAIL_NOT_FOUND);
+    error.status = status.FORBIDDEN;
+    throw error;
+  }
+
+  const desencryptedPassword = await bcryptService.comparePassword(data.password, user.password);
+
+  if (!desencryptedPassword) {
+    const error = new Error(messages.NO_MATCH_USER);
+    error.status = status.NOT_FOUND_ERROR;
+    throw error;
+  }
+
+  const token = jwtService.generateToken(user);
+
+  return token;
+};
+
+module.exports = { create, login };
