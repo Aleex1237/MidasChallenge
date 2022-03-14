a7c732dfeeb1484f570f5b17c83ceadf57bfc1d3
Aleex1237 <alexis10893@outlook.com>
2022-03-13 21:38:43 -0300

    applied authMiddleware for movies endpoints

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 
+const authMiddleware = require('../middlewares/auth');
 /* GET home page. */
-router.use('/movies', moviesRouter);
+router.use('/movies', authMiddleware.authUser, moviesRouter);
 router.use('/users', usersRouter);
 
 module.exports = router;
